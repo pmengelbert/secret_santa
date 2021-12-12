7fab0e76b4ea35df81744520ac82c1cc7426c1b0
Peter Engelbert <pmengelbert@gmail.com>
2021-12-12 13:41:19 -0600

    It is pretty close. I'm fairly certain there are no incorrect results
    Signed-off-by: Peter Engelbert <pmengelbert@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,130 @@
 use rand::Rng;
-use std::collections::BTreeSet;
 use std::collections::HashMap;
+use std::collections::HashSet;
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy, Hash)]
 enum Sibling {
-    Meghan,
-    Greg,
-    Michael,
-    Kirsten,
-    Mark,
-    Lina,
-    Peter,
-    Claire,
-    John,
-    Colin,
+    Alice,
+    Bob,
+    Charlie,
+    Dan,
+    Ethyl,
+    Fred,
 }
+
+const SIBLINGS: [Sibling; 6] = [
+    Sibling::Alice,
+    Sibling::Bob,
+    Sibling::Charlie,
+    Sibling::Dan,
+    Sibling::Ethyl,
+    Sibling::Fred,
+];
 
 fn main() {
     use Sibling::*;
-    let siblings = [
-        Meghan, Greg, Michael, Kirsten, Mark, Lina, Peter, Claire, John, Colin,
-    ];
 
-    let mut vsiblings = Vec::from_iter(siblings.clone().into_iter());
+    let mut givers: HashSet<Sibling> = HashSet::new();
+    let mut receivers: HashSet<Sibling> = HashSet::new();
 
-    // nobody should get their spouse
+    let mut final_results: HashSet<Vec<(Sibling, Sibling)>> = HashSet::new();
+    let mut result: Vec<Sibling> = Vec::new();
+    let mut other_result: HashSet<(Sibling, Sibling)> = HashSet::new();
+
     let mut verboten: HashMap<Sibling, Sibling> = HashMap::new();
-    verboten.insert(Meghan, Greg);
-    verboten.insert(Greg, Meghan);
-    verboten.insert(Michael, Kirsten);
-    verboten.insert(Kirsten, Michael);
-    verboten.insert(Mark, Lina);
-    verboten.insert(Lina, Mark);
-    verboten.insert(Peter, Claire);
-    verboten.insert(Claire, Peter);
-    verboten.insert(John, Colin);
-    verboten.insert(Colin, John);
+    verboten.insert(Alice, Bob);
+    verboten.insert(Bob, Alice);
+    verboten.insert(Charlie, Dan);
+    verboten.insert(Dan, Charlie);
+    verboten.insert(Ethyl, Fred);
+    verboten.insert(Fred, Ethyl);
 
-    // nobody should get assigned as a receiver more than once, so
-    // we need to keep track of who has been "used up" already
-    let mut taken = BTreeSet::new();
+    r(
+        &mut final_results,
+        &mut result,
+        &mut other_result,
+        0,
+        &mut givers,
+        &mut receivers,
+        &verboten,
+    );
 
-    let mut matchups: HashMap<Sibling, Sibling> = HashMap::new();
-    for giving in siblings {
-        let mut index = rand::thread_rng().gen_range(0..vsiblings.len());
-        let mut receiving = vsiblings.remove(index);
-        while Some(&receiving) == verboten.get(&giving) || giving == receiving {
-            vsiblings.push(receiving);
-            index = rand::thread_rng().gen_range(0..vsiblings.len());
-            receiving = vsiblings.remove(index);
+    let mut count = 0;
+    for result in final_results {
+        count += 1;
+        for (g, r) in result {
+            println!("{:?} gives to {:?}", g, r);
         }
-        matchups.insert(giving, receiving);
-        taken.insert(receiving);
+        println!("");
     }
 
-    for k in matchups.keys() {
-        println!(
-            "{:?}\t\twill give a gift to\t\t{:?}",
-            k,
-            *matchups.get(&k).unwrap()
-        );
+    println!("total of {} results", count);
+}
+
+fn r(
+    final_result: &mut HashSet<Vec<(Sibling, Sibling)>>,
+    result: &mut Vec<Sibling>,
+    other_result: &mut HashSet<(Sibling, Sibling)>,
+    depth: usize,
+    givers: &mut HashSet<Sibling>,
+    receivers: &mut HashSet<Sibling>,
+    verboten: &HashMap<Sibling, Sibling>,
+) {
+    if depth == SIBLINGS.len() * 2 {
+        let mut new_result = Vec::new();
+        for r in other_result.iter() {
+            new_result.push(*r);
+        }
+        new_result.sort();
+        final_result.insert(new_result);
+        return;
+    }
+
+    // even
+    if depth & 1 == 0 {
+        for sibling in &SIBLINGS {
+            if !givers.contains(sibling) {
+                givers.insert(*sibling);
+                result.push(*sibling);
+
+                r(
+                    final_result,
+                    result,
+                    other_result,
+                    depth + 1,
+                    givers,
+                    receivers,
+                    verboten,
+                );
+                givers.remove(sibling);
+                result.pop();
+            }
+        }
+    } else {
+        for sibling in &SIBLINGS {
+            let last = result[result.len() - 1];
+            if last == *sibling || verboten.get(&last) == Some(sibling) {
+                continue;
+            }
+
+            if !receivers.contains(sibling) {
+                receivers.insert(*sibling);
+                result.push(*sibling);
+                other_result.insert((last, *sibling));
+
+                r(
+                    final_result,
+                    result,
+                    other_result,
+                    depth + 1,
+                    givers,
+                    receivers,
+                    verboten,
+                );
+                receivers.remove(sibling);
+                other_result.remove(&(last, *sibling));
+                result.pop();
+            }
+        }
     }
 }
