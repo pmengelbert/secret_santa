0cd063d6031522a32933f6924109cc3db20ac647
Peter Engelbert <pmengelbert@gmail.com>
2022-01-13 22:19:49 -0600

    breadcrumb
    Signed-off-by: Peter Engelbert <pmengelbert@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![allow(unused_variables, dead_code, unused_imports)]
 use std::collections::HashMap;
 use std::collections::HashSet;
+use std::collections::VecDeque;
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy, Hash)]
 enum Sibling {
@@ -26,54 +27,94 @@ fn main() {
     use Participant::*;
     use Sibling::*;
 
-    let givers = vec![
-        Giver(Meghan),
-        Giver(Michael),
-        Giver(Mark),
-        Giver(Peter),
-        Giver(John),
-        Giver(Greg),
-        Giver(Kirsten),
-        Giver(Lina),
-        Giver(Claire),
-        Giver(Colin),
-    ];
+    let mut givers = VecDeque::from_iter(
+        [Giver(Meghan), Giver(Michael), Giver(Greg), Giver(Kirsten)].into_iter(),
+    );
 
-    let receivers = vec![
-        Receiver(Meghan),
-        Receiver(Michael),
-        Receiver(Mark),
-        Receiver(Peter),
-        Receiver(John),
-        Receiver(Greg),
-        Receiver(Kirsten),
-        Receiver(Lina),
-        Receiver(Claire),
-        Receiver(Colin),
-    ];
+    let mut receivers = VecDeque::from_iter(
+        [
+            Receiver(Meghan),
+            Receiver(Michael),
+            Receiver(Greg),
+            Receiver(Kirsten),
+        ]
+        .into_iter(),
+    );
 
-    let participants = vec![
-        Giver(Meghan),
-        Giver(Michael),
-        Giver(Mark),
-        Giver(Peter),
-        Giver(John),
-        Giver(Greg),
-        Giver(Kirsten),
-        Giver(Lina),
-        Giver(Claire),
-        Giver(Colin),
-        Receiver(Meghan),
-        Receiver(Michael),
-        Receiver(Mark),
-        Receiver(Peter),
-        Receiver(John),
-        Receiver(Greg),
-        Receiver(Kirsten),
-        Receiver(Lina),
-        Receiver(Claire),
-        Receiver(Colin),
-    ];
+    let mut verboten = HashMap::new();
+    verboten.insert(Meghan, Meghan);
+    verboten.insert(Greg, Greg);
+    verboten.insert(Meghan, Greg);
+    verboten.insert(Greg, Meghan);
+
+    verboten.insert(Michael, Michael);
+    verboten.insert(Kirsten, Kirsten);
+    verboten.insert(Michael, Kirsten);
+    verboten.insert(Kirsten, Michael);
+
+    let mut builder = vec![];
+    let mut output = vec![];
+
+    let first = givers.pop_front().unwrap();
+    r(
+        &mut givers,
+        &mut receivers,
+        &mut builder,
+        &mut output,
+        &verboten,
+        (first, None),
+    );
 
     // take each giver in turn
+    // meghan
+}
+
+fn r(
+    givers: &mut VecDeque<Participant>,
+    receivers: &mut VecDeque<Participant>,
+    builder: &mut Vec<(Participant, Participant)>,
+    output: &mut Vec<Vec<(Participant, Participant)>>,
+    verboten: &HashMap<Sibling, Sibling>,
+    mut current: (Participant, Option<Participant>),
+) {
+    use Participant::*;
+
+    let participant = match current {
+        (g, None) => g,
+        (Giver(g), Some(Receiver(rcv))) => {
+            if let Some(&recv) = verboten.get(&g) {
+                if recv == rcv {
+                    return;
+                } else {
+                    builder.push((Giver(g), Receiver(rcv)));
+                    if builder.len() == 2 {
+                        output.push(builder.clone());
+                        return;
+                    }
+                    if let Some(next_giver) = givers.pop_front() {
+                        r(
+                            givers,
+                            receivers,
+                            builder,
+                            output,
+                            verboten,
+                            (next_giver, None),
+                        );
+                    } else {
+                        return;
+                    }
+                }
+            }
+            Receiver(rcv)
+        }
+        _ => unreachable!(),
+    };
+
+    // match participant {
+    //     Giver(g) => {
+    //         let mut frozen = receivers.clone();
+    //         while let Some(r) = frozen.pop_front() {}
+    //     }
+    //     Receiver(r) => {}
+    // }
 }
