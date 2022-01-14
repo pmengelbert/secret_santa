6ddf657d45b81cec13ac0d885f493dad4d8a5a41
Peter Engelbert <pmengelbert@gmail.com>
2022-01-13 21:38:11 -0600

    With 10 couples
    Signed-off-by: Peter Engelbert <pmengelbert@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,126 +1,79 @@
+#![allow(unused_variables, dead_code, unused_imports)]
 use std::collections::HashMap;
 use std::collections::HashSet;
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy, Hash)]
 enum Sibling {
-    Alice,
-    Bob,
-    Charlie,
-    Dan,
-    Ethyl,
-    Fred,
-    George,
-    Hannah,
+    Meghan,
+    Greg,
+    Michael,
+    Kirsten,
+    Mark,
+    Lina,
+    Peter,
+    Claire,
+    John,
+    Colin,
 }
 
-const SIBLINGS: [Sibling; 4] = [Sibling::Alice, Sibling::Bob, Sibling::Charlie, Sibling::Dan];
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy, Hash)]
+enum Participant {
+    Giver(Sibling),
+    Receiver(Sibling),
+}
 
 fn main() {
+    use Participant::*;
     use Sibling::*;
 
-    let mut givers: HashSet<Sibling> = HashSet::new();
-    let mut receivers: HashSet<Sibling> = HashSet::new();
+    let givers = vec![
+        Giver(Meghan),
+        Giver(Michael),
+        Giver(Mark),
+        Giver(Peter),
+        Giver(John),
+        Giver(Greg),
+        Giver(Kirsten),
+        Giver(Lina),
+        Giver(Claire),
+        Giver(Colin),
+    ];
 
-    let mut final_results: HashSet<Vec<(Sibling, Sibling)>> = HashSet::new();
-    let mut result: Vec<Sibling> = Vec::new();
-    let mut other_result: HashSet<(Sibling, Sibling)> = HashSet::new();
+    let receivers = vec![
+        Receiver(Meghan),
+        Receiver(Michael),
+        Receiver(Mark),
+        Receiver(Peter),
+        Receiver(John),
+        Receiver(Greg),
+        Receiver(Kirsten),
+        Receiver(Lina),
+        Receiver(Claire),
+        Receiver(Colin),
+    ];
 
-    let mut verboten: HashMap<Sibling, Sibling> = HashMap::new();
-    verboten.insert(Alice, Bob);
-    verboten.insert(Bob, Alice);
-    verboten.insert(Charlie, Dan);
-    verboten.insert(Dan, Charlie);
-    verboten.insert(Ethyl, Fred);
-    verboten.insert(Fred, Ethyl);
-    verboten.insert(George, Hannah);
-    verboten.insert(Hannah, George);
+    let participants = vec![
+        Giver(Meghan),
+        Giver(Michael),
+        Giver(Mark),
+        Giver(Peter),
+        Giver(John),
+        Giver(Greg),
+        Giver(Kirsten),
+        Giver(Lina),
+        Giver(Claire),
+        Giver(Colin),
+        Receiver(Meghan),
+        Receiver(Michael),
+        Receiver(Mark),
+        Receiver(Peter),
+        Receiver(John),
+        Receiver(Greg),
+        Receiver(Kirsten),
+        Receiver(Lina),
+        Receiver(Claire),
+        Receiver(Colin),
+    ];
 
-    r(
-        &mut final_results,
-        &mut result,
-        &mut other_result,
-        0,
-        &mut givers,
-        &mut receivers,
-        &verboten,
-    );
-
-    let mut count = 0;
-    for result in final_results {
-        count += 1;
-        for (g, r) in result {
-            println!("{:?} gives to {:?}", g, r);
-        }
-        println!("");
-    }
-
-    println!("total of {} results", count);
-}
-
-fn r(
-    final_result: &mut HashSet<Vec<(Sibling, Sibling)>>,
-    result: &mut Vec<Sibling>,
-    other_result: &mut HashSet<(Sibling, Sibling)>,
-    depth: usize,
-    givers: &mut HashSet<Sibling>,
-    receivers: &mut HashSet<Sibling>,
-    verboten: &HashMap<Sibling, Sibling>,
-) {
-    if depth == SIBLINGS.len() * 2 {
-        let mut new_result = Vec::new();
-        for r in other_result.iter() {
-            new_result.push(*r);
-        }
-        new_result.sort();
-        final_result.insert(new_result);
-        return;
-    }
-
-    // even
-    if depth & 1 == 0 {
-        for sibling in &SIBLINGS {
-            if !givers.contains(sibling) {
-                givers.insert(*sibling);
-                result.push(*sibling);
-
-                r(
-                    final_result,
-                    result,
-                    other_result,
-                    depth + 1,
-                    givers,
-                    receivers,
-                    verboten,
-                );
-                givers.remove(sibling);
-                result.pop();
-            }
-        }
-    } else {
-        for sibling in &SIBLINGS {
-            let last = result[result.len() - 1];
-            if last == *sibling || verboten.get(&last) == Some(sibling) {
-                continue;
-            }
-
-            if !receivers.contains(sibling) {
-                receivers.insert(*sibling);
-                result.push(*sibling);
-                other_result.insert((last, *sibling));
-
-                r(
-                    final_result,
-                    result,
-                    other_result,
-                    depth + 1,
-                    givers,
-                    receivers,
-                    verboten,
-                );
-                receivers.remove(sibling);
-                other_result.remove(&(last, *sibling));
-                result.pop();
-            }
-        }
-    }
+    // take each giver in turn
 }
